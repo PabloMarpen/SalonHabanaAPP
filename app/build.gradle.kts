e4e71b54ff0660af4a8e0d7768deb01885e04b92
PabloMarpen <pablom.marpen@educa.jcyl.es>
2024-11-12 00:04:10 +0100

    v2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.salonhabanaapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.salonhabanaapp"
-        minSdk = 25
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/com/example/salonhabanaapp/SecondActivity.kt
+++ app/src/main/java/com/example/salonhabanaapp/SecondActivity.kt
@@ -1,44 +1,89 @@
 package com.example.salonhabanaapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.SeekBar
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var spinnerEvento: Spinner
+    lateinit var spinnerFecha: Spinner
+    lateinit var spinnerCocina: Spinner
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
 
-        class SecondActivity : AppCompatActivity() {
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                enableEdgeToEdge()
-                setContentView(R.layout.activity_second)
+        spinnerEvento = findViewById(R.id.spinnerTipoEvento)
+        spinnerFecha = findViewById(R.id.spinnerFecha)
+        spinnerCocina = findViewById(R.id.spinnerTipoCocina)
+        val personas = findViewById<TextView>(R.id.textPersonas)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar2)
+        seekBar.min = 0
+        seekBar.max = 100
 
-                val spinnerFecha = findViewById<Spinner>(R.id.spinnerFecha)
-                val spinnerTipoEvento = findViewById<Spinner>(R.id.spinnerTipoEvento)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.event_types_array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerEvento.adapter = adapter
+        }
 
-                val adapterFecha = ArrayAdapter.createFromResource(
-                    this,
-                    R.array.dates_array,
-                    android.R.layout.simple_spinner_item
-                )
-                adapterFecha.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinnerFecha.adapter = adapterFecha
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.dates_array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerFecha.adapter = adapter
+        }
 
-                val adapterTipoEvento = ArrayAdapter.createFromResource(
-                    this,
-                    R.array.event_types_array,
-                    android.R.layout.simple_spinner_item
-                )
-                adapterTipoEvento.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinnerTipoEvento.adapter = adapterTipoEvento
-            }
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.tipo_cocina,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerCocina.adapter = adapter
         }
 
 
+        spinnerEvento.onItemSelectedListener = this
+        spinnerFecha.onItemSelectedListener = this
+        spinnerCocina.onItemSelectedListener = this
+
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            /**
+             * Método que se llama cada vez que cambio el progreso
+             * seekBar contiene la referencia a la seekBar
+             * progress contiene el valor actual de la seekbar
+             * fromUser indica si el cambio lo ha producido el usuario al arastar la barra
+             */
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                personas.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
     }
 }
